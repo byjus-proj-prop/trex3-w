5d59d219679d64805ea8e33948bb63ea5c594e9e
idks9 <70767789+idks9@users.noreply.github.com>
2022-01-31 19:39:12 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -70,7 +70,7 @@ function setup() {
 
 function draw() {
   //trex.debug = true;
-  background(140);
+  background(bg);
   trex.x=camera.position.x-250;
   text("Pontuação: "+ pontuacao, width-125,50);
   
